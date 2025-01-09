f46ab49ee9d5658ecd3da8deb91137fe46b8f316
omarderas <bz.inlandtours@gmail.com>
2025-01-09 00:08:48 -0600

    first push

--- src/components/header.js
+++ src/components/header.js
@@ -11,7 +11,7 @@ const MainLogo = "../images/outdoor-logo.png";
 const Header = ({ siteTitle }) => (
   <header>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" title="" href="/">  <StaticImage src="../images/outdoor-logo.png" quality={60} formats={["auto", "webp", "avif"]} /></a>
+  <a class="navbar-brand" title="" href="/">  <StaticImage src="../images/outdoor-logo.png" quality={60} formats={["auto"]} /></a>
   
 <div className="contact-block">
 
